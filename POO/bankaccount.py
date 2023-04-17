9b9c507fd1cfbef8ae1e72a64027464d2888cf90
Pedro Zupo <zupinhou@gmail.com>
2023-04-17 17:58:18 -0300

    Adicionando o método 'transfer' e outras modificações
    Método 'transfer' adicionado ao código, variáveis renomeadas e corrigido o erro do método '__str__'.

--- POO/bankaccount.py
+++ POO/bankaccount.py
@@ -1,24 +1,29 @@
 class account:
-    def __init__(self, id, name, balance):
-        self.__id = id
-        self.__name = name
-        self.__balance = balance
+    def __init__(self, account_id, account_holder_name, account_balance):
+        self.__account_id = account_id
+        self.__account_holder_name = account_holder_name
+        self.__account_balance = account_balance
     
     def deposit(self, amount):
         if amount <= 0:
             print('O valor depositado deve ser positivo!')    
-        self.__balance += amount
+        self.__account_balance += amount
 
     def withdraw(self, amount):
         if amount <= 0:
             print('O valor a ser sacado deve ser positivo!')
-        elif self.__balance <= amount:
+        elif self.__account_balance <= amount:
             print('Saldo insuficiente!')
         else:
-            self.__balance -= amount
+            self.__account_balance -= amount
+
+    def transfer(self, destination_account, amount):
+        self.withdraw(amount)
+        destination_account.deposit(amount)
+        print(f'A tranferência de {amount:.2f} foi realizada com sucesso!')
     
-    def show_balance(self):
-        print(f'Seu saldo é: {self.__balance}')
+    def show_account_balance(self):
+        print(f'Seu saldo é: {self.__account_balance}')
 
     def __str__(self):
-        return print(f'Conta:#{self.__id}{self.__name} | Saldo:{self.__balance}')
+        return f'Conta:# {self.__account_id}{self.__account_holder_name} | Saldo:{self.__account_balance}'

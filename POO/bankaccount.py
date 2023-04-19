9647a4cf8ac3a56e7331e12f605e97b998203b55
Pedro Zupo <zupinhou@gmail.com>
2023-04-19 18:10:03 -0300

    Adicionando métodos 'getters' e melhorias no código
    O método '__str__' foi retirado do código e impressões de erro foram substituídas pelo 'ValueError'.

--- POO/bankaccount.py
+++ POO/bankaccount.py
@@ -6,24 +6,35 @@ class account:
     
     def deposit(self, amount):
         if amount <= 0:
-            print('O valor depositado deve ser positivo!')    
+            raise ValueError('O valor depositado deve ser positivo!')
         self.__account_balance += amount
 
     def withdraw(self, amount):
         if amount <= 0:
-            print('O valor a ser sacado deve ser positivo!')
+            raise ValueError('O valor a ser sacado deve ser positivo!')
         elif self.__account_balance <= amount:
-            print('Saldo insuficiente!')
+            raise ValueError('Saldo insuficiente!')
         else:
             self.__account_balance -= amount
 
     def transfer(self, destination_account, amount):
+        if self.__account_id == destination_account.get_account_id():
+            raise ValueError('Não é possível transferir para a mesma conta!')
         self.withdraw(amount)
         destination_account.deposit(amount)
         print(f'A tranferência de {amount:.2f} foi realizada com sucesso!')
     
     def show_account_balance(self):
         print(f'Seu saldo é: {self.__account_balance}')
-
-    def __str__(self):
-        return f'Conta:# {self.__account_id}{self.__account_holder_name} | Saldo:{self.__account_balance}'
+    
+    @property
+    def account_id(self):
+        return self.__account_id
+    
+    @property
+    def account_holder_name(self):
+        return self.__account_holder_name
+    
+    @property
+    def account_balance(self):
+        return self.__account_balance

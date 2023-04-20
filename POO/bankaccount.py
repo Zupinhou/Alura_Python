21f1e75aa1953fa3277ec53bc6f5bc425e117054
Pedro Zupo <zupinhou@gmail.com>
2023-04-19 21:09:48 -0300

    Adicionando verificação de tipos de dados aos argumentos dos métodos usando a biblioteca 'typing'

--- POO/bankaccount.py
+++ POO/bankaccount.py
@@ -1,15 +1,17 @@
+from typing import Union
+
 class account:
-    def __init__(self, account_id, account_holder_name, account_balance):
+    def __init__(self, account_id: str, account_holder_name: str, account_balance: float):
         self.__account_id = account_id
         self.__account_holder_name = account_holder_name
         self.__account_balance = account_balance
     
-    def deposit(self, amount):
+    def deposit(self, amount: Union[int, float]) -> None:
         if amount <= 0:
             raise ValueError('O valor depositado deve ser positivo!')
         self.__account_balance += amount
 
-    def withdraw(self, amount):
+    def withdraw(self, amount: Union[int, float]) -> None:
         if amount <= 0:
             raise ValueError('O valor a ser sacado deve ser positivo!')
         elif self.__account_balance < amount:
@@ -17,7 +19,7 @@ class account:
         else:
             self.__account_balance -= amount
 
-    def transfer(self, destination_account, amount):
+    def transfer(self, destination_account: "account", amount: Union[int, float]) -> None:
         if amount <= 0:
             raise ValueError('O valor a ser transferido deve ser positivo!')
         elif self.__account_balance < amount:
@@ -29,17 +31,17 @@ class account:
             destination_account.deposit(amount)
             print(f'A tranferência de {amount:.2f} foi realizada com sucesso!') 
     
-    def show_account_balance(self):
+    def show_account_balance(self) -> None:
         print(f'Seu saldo é: {self.__account_balance}')
     
     @property
-    def account_id(self):
+    def account_id(self) -> str:
         return self.__account_id
     
     @property
-    def account_holder_name(self):
+    def account_holder_name(self) -> str:
         return self.__account_holder_name
     
     @property
-    def account_balance(self):
+    def account_balance(self) -> float:
         return self.__account_balance
